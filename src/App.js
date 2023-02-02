4c0050a9d48cbfeceb1264b28d8d54495a3e6a80
adem kalfalı <ademkalfa9@gmail.com>
2023-02-02 14:18:15 +0300

    checkout_update

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
         <Header />
 
         <Switch>
+          <Route path="/login">
+            <h1>Login page</h1>
+          </Route>
           <Route path="/checkout">
             <Checkout />
           </Route>
