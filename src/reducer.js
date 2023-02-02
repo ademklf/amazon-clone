46cf7c5938a999924a693d672c4066ecaa7c9591
adem kalfalı <ademkalfa9@gmail.com>
2023-02-02 14:02:22 +0300

    removeBasket_add

--- src/reducer.js
+++ src/reducer.js
@@ -15,6 +15,12 @@ const reducer = (state, action) => {
         basket: [...state.basket, action.item],
       };
 
+    case "REMOVE_FROM_BASKET":
+      return {
+        ...state,
+        basket: state.basket.filter((item) => item.id !== action.id),
+      };
+
     default:
       return state;
   }
