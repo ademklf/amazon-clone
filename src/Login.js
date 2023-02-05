46c1990296fe89b821b9a4d0a588206973082551
adem kalfalı <ademkalfa9@gmail.com>
2023-02-05 10:32:54 +0300

    login_update

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    // some fancy firebase login shitttt....
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    // do some fancy firebase register shittt...
+  };
   return (
     <div className="login">
       <Link to="/">
@@ -12,6 +24,42 @@ function Login() {
           alt=""
         />
       </Link>
+      <div className="login__container">
+        <h1>Sign İn</h1>
+
+        <form action="">
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            onClick={signIn}
+            className="login__signInButton"
+          >
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to the AMAZON-CLONE Conditions of Use & Sale.
+          Please see our Privacy Notice, our Cookies Notice and our
+          Interest-Based Ads Notice.
+        </p>
+
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon account
+        </button>
+      </div>
     </div>
   );
 }
