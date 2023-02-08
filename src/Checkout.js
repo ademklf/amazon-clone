46d26149b61d5ceb8aca10c0f6e3acbca4789cec
adem kalfalı <ademkalfa9@gmail.com>
2023-02-08 03:19:46 +0300

    hosting add

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,7 +6,7 @@ import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -17,6 +17,7 @@ function Checkout() {
           alt=""
         />
         <div>
+          <h3>Hello, {user ? user.email : "Guest"}</h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
 
           {basket.map((item) => (
